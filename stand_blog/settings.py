c4422e145dc0cf115ac433ddc6b05b37ccfa8a76
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2022-10-17 16:16:06 +0330

    add home add and first view

--- stand_blog/settings.py
+++ stand_blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my app
     'blog.apps.BlogConfig',
+    'home_app.apps.HomeAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS= [os.path.join(BASE_DIR, 'assets')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
