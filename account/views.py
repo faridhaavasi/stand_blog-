5094d90855c67040c46d6132d50e3cf82e952fa8
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2022-10-20 16:36:24 +0330

    login view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 # Create your views here.
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+
+    return render(request, 'account/login.html', {})
