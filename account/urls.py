7c0a63adfe8c7a45c304b9618642d9eb1fcba53c
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2022-10-28 15:25:24 +0330

    l1 register

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login',views.login_user),
+    path('login', views.login_user),
+    path('logout', views.logout_user),
+    path('register', views.register),
 
 ]
--- account/views.py
+++ account/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
 # Create your views here.
 def login_user(request):
+    if request.user.is_authenticated is True:
+        return redirect('/')
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -13,3 +15,10 @@ def login_user(request):
             return redirect('/')
 
     return render(request, 'account/login.html', {})
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def register(request):
+    return render(request ,'account/register.html', {})
