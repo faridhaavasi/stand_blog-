02ad0e322f941f32c3c711a09d8de229a3407c62
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2022-10-29 13:23:11 +0330

    final register

--- account/views.py
+++ account/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
@@ -21,4 +21,19 @@ def logout_user(request):
     return redirect('/')
 
 def register(request):
-    return render(request ,'account/register.html', {})
+    context = {
+        'errors': [],
+    }
+    if request.user.is_authenticated is True:
+        return redirect('/')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password1 != password2:
+            context['errors'].append('passwords is not match')
+            return render(request, 'account/register.html', context)
+        user = User.objects.create(username=username, password=password1)
+        login(request, user)
+
+    return render(request, 'account/register.html', {})
