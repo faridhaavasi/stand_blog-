2124a12f57e5863b234af82d31f375f272dbb17b
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2022-11-03 02:10:17 +0330

    added category models and see bug 2 in template index.html category {{post.category,all}}

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    activate = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('d', 'draft'),
@@ -10,6 +18,7 @@ class Post(models.Model):
     )
     other = models.ForeignKey(User, on_delete=models.CASCADE) # many to one
     title = models.CharField(max_length=20)
+    category = models.ManyToManyField(Category, null=True)
     body = models.TextField()
     img = models.ImageField(upload_to='images/blog/post')
     created = models.DateTimeField(auto_now=timezone)
