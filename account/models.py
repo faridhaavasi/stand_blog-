15cce8ec057037d7d88cd98af0e2291dfaf36132
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2022-11-05 00:43:02 +0330

    add modeld profile

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    user = models.ManyToManyField(User)
+    img = models.ImageField(upload_to='account/profile')
+    mellicode = models.CharField(max_length=11, unique=True)
+
+    def __str__(self):
+        return self.mellicode
