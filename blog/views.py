6451db83d4093636c9bad97c0fb2ecf82707bc44
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2022-11-06 23:12:17 +0330

    detail view v1

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 # Create your views here.
 
+def detail(request, pk):
+    
+    return render(request, 'blog/detail.html', {})
