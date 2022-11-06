337bfee5e21269efb6be0d3ef584b2bdbad72968
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2022-11-06 23:32:21 +0330

    added managers

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,20 @@ from datetime import timezone
 from django.contrib.auth.models import User
 from django.db import models
 
+# my managers
+class Post_Manager(models.Manager):
+    def published(self):
+        return self.filter(status='p')
+
+class Category_Managert(models.Manager):
+    def activated(self):
+        return self.filter(activate=True)
+
 # Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length=100)
     activate = models.BooleanField(default=True)
+    objects = Category_Managert()
 
     def __str__(self):
         return self.title
@@ -24,6 +34,6 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now=timezone)
     updated = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=1, default='d', choices=STATUS_CHOICES, null=False)
-
+    objects = Post_Manager()
     def __str__(self):
         return self.title
--- home_app/views.py
+++ home_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from blog.models import Post
 # Create your views here.
 def home(request):
-    posts = Post.objects.all()
+    posts = Post.objects.published()
     return render(request, 'home_app/index.html', {'posts': posts})
