5720ab413187abaa2e81103a7a97da0f29df182d
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2022-11-14 16:45:21 +0330

    category context processors

--- context_processors/context_processors.py
+++ context_processors/context_processors.py
@@ -1,6 +1,12 @@
-from blog.models import Post
+from blog.models import Post, Category
 
 def recent_posts(request):
     recent_post = Post.objects.order_by('-created')[:3]
 
     return {'recent_post': recent_post}
+
+def category_list(request):
+
+    category = Category.objects.activated()
+
+    return {'category': category}
