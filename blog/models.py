1eaef72bdf3c0fff3b015465e908afc643233f8c
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2022-11-18 20:46:12 +0330

    comment part1

--- blog/models.py
+++ blog/models.py
@@ -56,3 +56,14 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['updated',]
+
+
+class Comment(models.Model):
+    posts = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE,null=True, blank=True, related_name='reply')
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[:50]
