cb68f510678022971e549723e4324266a31cca2b
faridhaavasi <faridhavasi777@gmail.com>
2022-11-27 21:56:44 +0330

    reset login ===test ok

---
+++ account/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+from django.contrib.auth import authenticate ,login
+
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('home_app:home')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(User, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home_app:home')
+    return render(request, 'account/login.html', {})
