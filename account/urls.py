74e12cbb0da01331d416e9fef695da098b6a42b1
faridhaavasi <faridhavasi777@gmail.com>
2022-11-27 20:17:31 +0330

    reset login and register

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,3 @@
 from django.urls import path
 from . import views
 app_name = 'account'
-urlpatterns = [
-    path('login', views.login_user, name='login'),
-    path('logout', views.logout_user, name='logout'),
-    path('register', views.register, name='register'),
-
-]
