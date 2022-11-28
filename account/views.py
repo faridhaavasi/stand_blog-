e1079be515d5f120be5476e3f26bf30971f0d74c
faridhaavasi <faridhavasi777@gmail.com>
2022-11-28 14:19:02 +0330

    rest register test ok

--- account/views.py
+++ account/views.py
@@ -18,3 +18,19 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return redirect('home_app:home')
+
+def register_user(request):
+    if request.user.is_authenticated:
+        return redirect('home_app:home')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        user = User.objects.create(username=username, password=password1)
+        if username and password1:
+            login(request, user)
+            return redirect('home_app:home')
+
+    return render(request, 'account/register.html')
+
+
