c96f14d3ff82b5bdc33c38a323cf94b6817d1e4d
faridhaavasi <faridhavasi777@gmail.com>
2022-12-08 02:58:11 +0330

    reset login by forms

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,30 @@
 from django import forms
 from django.contrib.auth.models import User
-
+from django.core.validators import ValidationError
+from django.contrib.auth import authenticate
 class loginform(forms.Form):
-    username = forms.CharField(max_length=20)
-    password = forms.CharField(max_length=30)
+    username = forms.CharField(max_length=20, widget=forms.TextInput(
+        attrs={'class': 'input100',
+               'placeholder': 'username',
+               }
+        ))
+    password = forms.CharField(max_length=30, widget=forms.PasswordInput(
+        attrs={
+            'class': 'input100',
+            'placeholder': 'password'
+             }
+
+        ))
+
+    def clean_passwoord(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        user = authenticate(username=self.cleaned_data.get('username'), password=self.cleaned_data.get('password'))
+        if user is not None:
+            return password
+
+        else:
+            raise ValidationError('username or password invalid ')
+
+
 
--- account/views.py
+++ account/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-
+from .forms import loginform
 from django.contrib.auth import authenticate ,login, logout
 
 def login_user(request):
     if request.user.is_authenticated:
         return redirect('home_app:home')
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(User, username=username, password=password)
-        if user is not None:
+        form = loginform(data=request.POST)
+        if form.is_valid():
+            user = User.objects.get(username=form.cleaned_data.get('username'))
+
             login(request, user)
             return redirect('home_app:home')
-    return render(request, 'account/login.html', {})
+    else:
+        form = loginform()
+
+
+    return render(request, 'account/login.html', {'form': form})
 
 def logout_user(request):
     logout(request)
