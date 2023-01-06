1d8435819a37a2e4e0d58bef9c286601ecc752eb
faridhaavasi <faridhavasi777@gmail.com>
2023-01-06 17:14:10 +0330

    add action

--- blog/admin.py
+++ blog/admin.py
@@ -9,6 +9,12 @@ class Post_admin(admin.ModelAdmin):
     list_filter = ('title', 'created')
     search_fields = ('title', 'created')
     list_editable = ('status',)
+    actions = ['make_published']
+
+    @admin.action(description='نتشر کردن آیتم های انتخابی')
+    def make_published(self, request, queryset):
+        queryset.update(status='p')
+
 
 
 
